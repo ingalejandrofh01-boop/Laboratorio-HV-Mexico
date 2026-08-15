ddf6ba2364e5ec028682ab66bdfe7446b70a871b
ingalejandrofh01-boop <ing.alejandrofh01@gmail.com>
2026-08-14 23:55:30 -0600

    update

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Habilita la instalación como app y un caché básico para que el sitio
 // cargue más rápido y funcione si la conexión falla momentáneamente.
 
-const CACHE_NAME = 'labhv-cache-v18';
+const CACHE_NAME = 'labhv-cache-v20';
 const CORE_ASSETS = [
   './',
   './index.html',
